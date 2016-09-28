ec99c3f7053bfd5be4b15bb853611ec63be25f5d
sakamossan <mossan@fout.jp>
2016-09-28 19:53:31 +0900

    add lang

---
+++ inits/01-lang-mode.el
@@ -0,0 +1,4 @@
+(setq auto-mode-alist (cons '("\\.hql$" . sql-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.scala$" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.ts$" . js-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.tsx$" . js-mode) auto-mode-alist))
--- inits/90-git-gutter.el
+++ inits/90-git-gutter.el
@@ -1,2 +1,3 @@
 (require 'git-gutter)
+
 (global-git-gutter-mode t)
