c245bc7474f487fcf4723e700bfc3a86bfe7b454
sakamoto <mossan@fout.jp>
2017-09-22 12:27:49 +0900

    add terraform mode

--- init.el
+++ init.el
@@ -41,3 +41,19 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(foreign-regexp/regexp-type (quote perl))
+ '(package-selected-packages
+   (quote
+    (hcl-mode color-theme yaml-mode ssh-config-mode recentf-ext rainbow-delimiters popwin init-loader helm git-link git-gutter foreign-regexp expand-region auto-complete)))
+ '(reb-re-syntax (quote foreign-regexp)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
